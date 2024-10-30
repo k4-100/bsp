59770527eb24bed3590c7b591db96589af204146
k4-100 <kmakingk@gmail.com>
2024-10-30 22:26:14 +0100

    barebone map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("\n");
+
+    let map: String = (0..30)
+        .map(|_| {
+            let row: String = (0..80).map(|_| ".").collect();
+            format!("{row}\n")
+        })
+        .collect();
+
+    println!("{}", map);
 }
