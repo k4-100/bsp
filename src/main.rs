e0c2bd740f28112a458a91c3dd230a4b381f2c87
k4-100 <kmakingk@gmail.com>
2024-11-01 16:28:23 +0100

    started implementing area spliting

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::{
     collections::{BTreeMap, HashMap},
     thread, time,
 };
+
+use rand::Rng;
+
+const X_LENGTH: usize = 120;
+const Y_LENGTH: usize = 40;
+
 #[derive(Debug)]
 struct Cell {
     pub x: u8,
@@ -35,24 +41,38 @@ impl Cell {
     // }
 }
 
-// pub fn split_rooms()
+pub fn split_rooms(mut data_map: &mut [[&str; X_LENGTH]; Y_LENGTH]) {
+    let mut rng = rand::thread_rng();
+    let mut vertical_split: bool = rand::random();
 
-const Y_LENGTH: usize = 40;
-const X_LENGTH: usize = 120;
+    if vertical_split {
+        let x: usize = rng.gen_range(20..X_LENGTH - 20);
+
+        for i in 0..data_map.len() {
+            data_map[i][x] = "#";
+        }
+
+        return;
+    }
+
+    let y: usize = rng.gen_range(6..Y_LENGTH - 6);
+
+    for i in 0..data_map[y].len() {
+        data_map[y][i] = "#";
+    }
+}
 
 fn main() {
     println!("\n");
 
-    // setup
+    //setup
 
     let mut data_map: [[&str; X_LENGTH]; Y_LENGTH] = [["."; X_LENGTH]; Y_LENGTH];
     let mut i: usize = 0;
 
-    // main execution loop
+    //main execution loop
     loop {
-        thread::sleep(time::Duration::from_millis(1000));
-        println!("iteration {i}");
-
+        split_rooms(&mut data_map);
         let displayed_map: String = (0..Y_LENGTH)
             .map(|y| {
                 let row: String = (0..X_LENGTH)
@@ -65,7 +85,10 @@ fn main() {
             })
             .collect();
 
+        println!("iteration {i}");
         println!("{}", displayed_map);
+
         i += 1;
+        thread::sleep(time::Duration::from_millis(1000));
     }
 }
